ff3ba20b25ab1a647ba839e3cae838e0e6417275
VeyronSakai <yuukisakai123@gmail.com>
2021-01-16 17:06:14 +0900

    fix CameraBase

--- com.veyronsakai.unipresentation/UniPresentation/Cameras/CameraBase.cs
+++ com.veyronsakai.unipresentation/UniPresentation/Cameras/CameraBase.cs
@@ -1,9 +1,20 @@
 ﻿using PrefabGenerator;
+using UnityEngine;
 
 namespace UniPresentation.Cameras
 {
     public abstract class CameraBase : PrefabBase, ICamera
     {
-        public abstract UnityEngine.Camera GetRawCamera();
+        private Camera _rawCamera;
+
+        public Camera GetRawCamera()
+        {
+            if (_rawCamera == null)
+            {
+                _rawCamera = GetComponent<Camera>();
+            }
+
+            return _rawCamera;
+        }
     }
 }
