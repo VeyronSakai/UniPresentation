96099826763eae972ac8afb0e588de3852b9d059
VeyronSakai <yuukisakai123@gmail.com>
2021-01-16 11:39:21 +0900

    add CavasContainer

---
+++ com.veyronsakai.unipresentation/UniPresentation/Canvases/CanvasContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace UniPresentation.Canvases
+{
+    public class CanvasContainer : ICanvasContainer
+    {
+        public List<ICanvas> Canvases { get; }
+
+        public CanvasContainer(List<ICanvas> canvases)
+        {
+            Canvases = canvases;
+        }
+    }
+}
